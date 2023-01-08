b0751515a5a6e6728889801813c8dbbb957e2747
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2023-01-08 11:33:56 +0100

    Remove all Firefox snap packages...

--- 22-04.sh
+++ 22-04.sh
@@ -187,7 +187,7 @@ fi
 
 paketerec="digikam exiv2 kipi-plugins graphicsmagick-imagemagick-compat"
 pakete=`echo "$pakete synaptic krita-l10n ubuntu-restricted-extras pidgin pinta nfs-common language-pack-kde-de libdvd-pkg smartmontools unoconv mediathekview python3-axolotl python3-gnupg gnome-software gnome-software-plugin-flatpak language-pack-de fonts-symbola vlc libxvidcore4 libfaac0 gnupg2 lutris dayon kate konsole element-desktop redshift-gtk firefox-locale-de firefox"`
-remove=`echo "$remove firefox"`
+remove=`echo "$remove firefox*"`
 
 sudo snap remove firefox
 sudo apt remove -y $remove
